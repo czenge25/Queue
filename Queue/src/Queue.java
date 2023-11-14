a6f235f26fe35fbf0f4186d846311213df4b9091
czenge25 <czenge25@kentdenver.org>
2023-11-14 09:47:22 -0700

    Commented and created headers

--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -1,49 +1,63 @@
+/*
+CZ
+11/17/23
+HK
+ */
+
 public class Queue <T> {
 
+    // Private instance data for the size and references to the front and back nodes
     private int size;
     private QueueNode<T> front = new QueueNode<>();
     private QueueNode<T> back = new QueueNode<>();
 
+    // Constructor for creating an empty queue
     public Queue() {
 
     }
 
+    // Method to add an element to the back of the queue
     public void enqueue(T element) {
-        QueueNode<T> n = new QueueNode<>(element);
+        QueueNode<T> newNode = new QueueNode<>(element);
+        // If the queue is empty, set the front to the new node
         if (isEmpty()) {
-            front = n;
+            front = newNode;
         } else {
-            back.setChild(n);
+            // Otherwise, set the child of the current back node to the new node
+            back.setChild(newNode);
         }
-        back = n;
+        // Update the back to be the new node, and increase the size
+        back = newNode;
         size++;
     }
 
+    // Method to remove and return the element from the front of the queue
     public T dequeue() {
+        // Check if the queue is not empty
         if (size >= 1) {
-            T data = front.getData();
+            T data = front.getData(); // Retrieve data from the front node
             front = front.getChild(); // Move the front reference to the next node
             size--;
             return data;
         }
+        // If the queue is empty, update the back reference as well
         if (front == null) {
-            back = null; // If the queue becomes empty, update the rear reference as well
+            back = null;
         }
-        return null;
+        return null; // Return null if the queue is empty
     }
 
+    // Method to check if the queue is empty
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return size == 0; // Return true if the size is 0, indicating an empty queue
     }
 
+    // Method to get the current size of the queue
     public int size() {
         return size;
     }
 
+    // Method to peek at the element in the front of the queue without removing it
     public T peek() {
         return front.getData();
     }
--- Queue/src/QueueNode.java
+++ Queue/src/QueueNode.java
@@ -1,48 +1,59 @@
+/*
+CZ
+11/17/23
+HK
+ */
+
 public class QueueNode<T> {
-    private T data; //Data stored in the node
-    private QueueNode<T> child; //Reference to the parent node
+    // Data stored in the node
+    private T data;
 
-    //Default constructor
+    // Reference to the child node
+    private QueueNode<T> child;
+
+    // Default constructor
     public QueueNode() {
-
+        // No additional initialization needed for an empty node
     }
 
-    //Constructor that sets data of the node
+    // Constructor that sets data of the node
     public QueueNode(T data) {
         this.data = data;
     }
 
-    //Constructor that sets data, and child
-    public QueueNode(T data, QueueNode child) {
+    // Constructor that sets data and child of the node
+    public QueueNode(T data, QueueNode<T> child) {
         this.data = data;
         this.child = child;
     }
 
-    public QueueNode getChild() {
+    // Gets the child node
+    public QueueNode<T> getChild() {
         return child;
     }
 
-    public void setChild(QueueNode n) {
+    // Sets the child node
+    public void setChild(QueueNode<T> n) {
         child = n;
     }
 
-    //Returns the data stored in the node
+    // Returns the data stored in the node
     public T getData() {
         return data;
     }
 
-    //Sets the data for this node
+    // Sets the data for this node
     public void setData(T data) {
         this.data = data;
     }
 
-    //Provides a string representation of the node by converting its data to a string
+    // Gives a string representation of the node by converting its data to a string
     public String toString() {
         return "Node: " + this.data.toString();
     }
 
-    //Compares the data of the node with the data of another node 'n'
-    public boolean equals(QueueNode n) {
+    // Compares the data of the node with the data of another node 'n'
+    public boolean equals(QueueNode<T> n) {
         return this.data.equals(n.getData());
     }
 }
